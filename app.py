fd183182acb9f60a5b93ee4551fbd524c9a610fe
Xavier Arce <xavierarce54321@gmail.com>
2025-05-18 16:21:09 +0200

    Update app.py to set host and port for deployment; add gunicorn to requirements

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask
 from routes.estimation import estimation_bp
 from flask_cors import CORS
+
 
 app = Flask(__name__)
 CORS(app)  # enables CORS for all routes
@@ -12,4 +14,5 @@ def home():
     return "API Flask funcionando!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # get PORT from env or default to 5000
+    app.run(host="0.0.0.0", port=port)
